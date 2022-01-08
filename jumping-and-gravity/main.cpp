f1ad92fc8851f45af0836322de732f35dc87ec97
thanhfphan <thanhptse@gmail.com>
2022-01-08 22:44:10 +0700

    add idle and running animation

--- jumping-and-gravity/main.cpp
+++ jumping-and-gravity/main.cpp
@@ -10,12 +10,21 @@ sf::IntRect area = sf::IntRect(159, 28, 384, 550);
 const sf::Time TIMER_PER_FRAME = sf::seconds(1.f / 60.f);
 sf::RenderWindow window(sf::VideoMode(WIDTH, HEIGHT), "SFML window");
 sf::Texture texture;
-sf::Sprite sprite;
-sf::Image santa;
+sf::Sprite player;
+sf::Image santaImage;
 sf::Vector2f gravity(0, 150);
 sf::Vector2f moveDirection(1, 1);
 sf::Vector2f velocity(0, 0);
 bool faceRight = true;
+bool isMoving = false;
+float duration = 0;
+
+sf::Texture runningRight[11];
+sf::Texture runningLeft[11];
+sf::Texture idleRight[16];
+sf::Texture idleLeft[16];
+int runningFrameCount = 0;
+int idleFrameCount = 0;
 
 void processEvent()
 {
@@ -66,9 +75,8 @@ void processEvent()
 
 void update()
 {
-	sf::Vector2f currentPosition = sprite.getPosition();
+	sf::Vector2f currentPosition = player.getPosition();
 	currentPosition.x += velocity.x * (float)15 * TIMER_PER_FRAME.asSeconds();
-	// std::cout << velocity.x << " " << velocity.y << std::endl;
 	if (currentPosition.y < HEIGHT - FIXVALUE)
 	{
 		currentPosition.y += gravity.y * (float)15 * TIMER_PER_FRAME.asSeconds();
@@ -92,63 +100,128 @@ void update()
 		currentPosition.x = WIDTH - FIXVALUE;
 	}
 
-	if (faceRight)
-	{
-		if (!santa.loadFromFile("images/Idle (1).png"))
-		{
-			std::cout << "error load file" << std::endl;
-			return;
-		}
+	isMoving = velocity.x != 0;
 
-		if (!texture.loadFromImage(santa, area))
-		{
-			std::cout << "error load image" << std::endl;
-			return;
-		}
+	player.setPosition(currentPosition.x, currentPosition.y);
+}
+
+void playerAnimation()
+{
+	if (runningFrameCount > (int)(sizeof(runningRight)/sizeof(*runningRight)-2))
+	{
+		runningFrameCount = 0;
 	}
 	else
 	{
-		if (!texture.loadFromImage(santa, area))
-		{
-			std::cout << "error load file" << std::endl;
-			return;
-		}
-		sf::Image flipImage = texture.copyToImage();
-		flipImage.flipHorizontally();
-		texture.update(flipImage);
-		sprite.setTexture(texture);
+		runningFrameCount += 1;
 	}
-	sprite.setPosition(currentPosition.x, currentPosition.y);
+	if (idleFrameCount > (int)(sizeof(idleRight)/sizeof(*idleRight)-2))
+	{
+		idleFrameCount = 0;
+	}
+	else
+	{
+		idleFrameCount += 1;
+	}
+
+	if (isMoving)
+	{
+		if (faceRight)
+		{
+			player.setTexture(runningRight[runningFrameCount]);
+		}
+		else
+		{
+			player.setTexture(runningLeft[runningFrameCount]);
+		}
+	}else{
+		if (faceRight)
+		{
+			player.setTexture(idleRight[idleFrameCount]);
+		}
+		else
+		{
+			player.setTexture(idleLeft[idleFrameCount]);
+		}
+	}
 }
 
 void render()
 {
 	window.clear();
-	window.draw(sprite);
+	window.draw(player);
 	window.display();
 }
 
 int main()
 {
-	if (!santa.loadFromFile("images/Idle (1).png"))
+
+	// moving animation
+	for (int i = 0; i < 11; ++i)
 	{
-		return EXIT_FAILURE;
+		std::string tmpName = "images/Run (" + std::to_string(i + 1) + ").png";
+		sf::Image tmpImg;
+		if (!tmpImg.loadFromFile(tmpName))
+		{
+			std::cout << "load from image failed " << std::endl;
+			return EXIT_FAILURE;
+		}
+
+		if (!runningRight[i].loadFromImage(tmpImg, area))
+		{
+			std::cout << "load from image failed " << std::endl;
+			return EXIT_FAILURE;
+		}
+		sf::Image flipImage = runningRight[i].copyToImage();
+		flipImage.flipHorizontally();
+		if (!runningLeft[i].loadFromImage(flipImage))
+		{
+			std::cout << "load from image failed " << std::endl;
+			return EXIT_FAILURE;
+		}
+	}
+	// idle animation
+	for (int i = 0; i < 16; ++i)
+	{
+		std::string tmpName = "images/Idle (" + std::to_string(i + 1) + ").png";
+		sf::Image tmpImg;
+		if (!tmpImg.loadFromFile(tmpName))
+		{
+			std::cout << "load from image failed " << std::endl;
+			return EXIT_FAILURE;
+		}
+
+		if (!idleRight[i].loadFromImage(tmpImg, area))
+		{
+			std::cout << "load from image failed " << std::endl;
+			return EXIT_FAILURE;
+		}
+		sf::Image flipImage = idleRight[i].copyToImage();
+		flipImage.flipHorizontally();
+		if (!idleLeft[i].loadFromImage(flipImage))
+		{
+			std::cout << "load from image failed " << std::endl;
+			return EXIT_FAILURE;
+		}
 	}
 
-	if (!texture.loadFromImage(santa, area))
-	{
-		return EXIT_FAILURE;
-	}
-
-	sprite.setTexture(texture);
-	sprite.setScale(sf::Vector2f(0.3, 0.3));
-	sprite.setPosition(sf::Vector2f(0, HEIGHT - FIXVALUE - 800));
+	player.setTexture(idleRight[0]);
+	player.setScale(sf::Vector2f(0.3, 0.3));
+	player.setPosition(sf::Vector2f(0, HEIGHT - FIXVALUE - 800));
 	sf::Time timeSinceLastUpdate = sf::Time::Zero;
 	sf::Clock clock;
 	while (window.isOpen())
 	{
 		sf::Time elapsedTime = clock.restart();
 		timeSinceLastUpdate += elapsedTime;
+
+		duration += elapsedTime.asSeconds();
+		if (duration > 0.1f)
+		{
+			duration = 0;
+			//update player animation
+			playerAnimation();
+		}
 
 		while (timeSinceLastUpdate > TIMER_PER_FRAME)
 		{
